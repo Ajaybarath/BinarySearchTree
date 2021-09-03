f41e73cb91b1ddfda75430acafb4d3cb38ed2abf
ajaybarath <ajaybarath3@gmail.com>
2021-09-03 16:03:14 +0530

    ADDED: to add and get the size of the tree

--- src/com/bridgeLabz/binarySearchTree/MyBinaryTreeTest.java
+++ src/com/bridgeLabz/binarySearchTree/MyBinaryTreeTest.java
@@ -20,4 +20,25 @@ class MyBinaryTreeTest {
 		Assert.assertEquals(true, result);
 	}
 
+	
+	@Test
+	public void addKeyAndGetSizeOfBinarySearchTree() {
+		
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+
+
+		
+		int result = myBinaryTree.getSize();
+	
+	
+		Assert.assertEquals(7, result);
+	}
+	
 }
