2505b419e97c391b43b6bfcd013dc53c9e51c940
ajaybarath <ajaybarath3@gmail.com>
2021-09-03 18:02:17 +0530

    ADDED: test method to verify search fuction

--- src/com/bridgeLabz/binarySearchTree/MyBinaryTreeTest.java
+++ src/com/bridgeLabz/binarySearchTree/MyBinaryTreeTest.java
@@ -33,12 +33,23 @@ class MyBinaryTreeTest {
 		myBinaryTree.add(60);
 		myBinaryTree.add(95);
 
-
-		
 		int result = myBinaryTree.getSize();
 	
-	
 		Assert.assertEquals(7, result);
+	}
+	
+	@Test
+	public void searchFromBinarySearchTree() {
+		
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		
+
+		int result = myBinaryTree.search(70);
+			
+		Assert.assertEquals(1, result);
 	}
 	
 }
